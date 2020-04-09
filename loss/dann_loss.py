63c2401e3181ab7928bf1621fa9698eefc74d5d2
Roman Shchedrin <rashchedrin@gmail.com>
2020-04-09 03:59:24 +0300

    renamed vars

--- loss/dann_loss.py
+++ loss/dann_loss.py
@@ -38,54 +38,54 @@ def _loss_DANN(
 
 # call loss_DANN instead of this function
 def _loss_DANN_splitted(
-        class_logits_on_source,
-        class_logits_on_target,
-        logprobs_target_on_source,
-        logprobs_target_on_target,
-        true_labels_on_source,
-        true_labels_on_target,
+        class_logits_on_src,
+        class_logits_on_trg,
+        logprobs_target_on_src,
+        logprobs_target_on_trg,
+        true_labels_on_src,
+        true_labels_on_trg,
         domain_loss_weight,
         prediction_loss_weight,
         unk_value=dann_config.UNK_VALUE,
 ):
     """
-    :param class_logits_on_source: Tensor, shape = (batch_size, n_classes).
-    :param class_logits_on_target: Tensor, shape = (batch_size, n_classes).
-    :param logprobs_target_on_source: Tensor, shape = (batch_size,):
-    :param logprobs_target_on_target: Tensor, shape = (batch_size,):
-    :param true_labels_on_source: np.Array, shape = (batch_size,)
-    :param true_labels_on_target: np.Array, shape = (batch_size,)
+    :param class_logits_on_src: Tensor, shape = (batch_size, n_classes).
+    :param class_logits_on_trg: Tensor, shape = (batch_size, n_classes).
+    :param logprobs_target_on_src: Tensor, shape = (batch_size,):
+    :param logprobs_target_on_trg: Tensor, shape = (batch_size,):
+    :param true_labels_on_src: np.Array, shape = (batch_size,)
+    :param true_labels_on_trg: np.Array, shape = (batch_size,)
     :param domain_loss_weight: weight of domain loss
     :param prediction_loss_weight: weight of prediction loss
     :param unk_value: value that means that true class label is unknown
     """
     # TARGET_DOMAIN_IDX is 1
-    source_len = len(class_logits_on_source)
-    target_len = len(class_logits_on_target)
-    true_labels_on_source = torch.Tensor(true_labels_on_source).long()
-    true_labels_on_target = torch.Tensor(true_labels_on_target).long()
-    is_target_on_source = torch.zeros(source_len).float()
-    is_target_on_target = torch.ones(target_len).float()
+    source_len = len(class_logits_on_src)
+    target_len = len(class_logits_on_trg)
+    true_labels_on_src = torch.Tensor(true_labels_on_src).long()
+    true_labels_on_trg = torch.Tensor(true_labels_on_trg).long()
+    is_target_on_src = torch.zeros(source_len).float()
+    is_target_on_trg = torch.ones(target_len).float()
 
     crossentropy = torch.nn.CrossEntropyLoss(ignore_index=unk_value, reduction='sum')
-    prediction_loss_on_source = crossentropy(class_logits_on_source, true_labels_on_source)
-    prediction_loss_on_target = crossentropy(class_logits_on_target, true_labels_on_target)
-    n_known = (true_labels_on_source != unk_value).sum() + \
-              (true_labels_on_target != unk_value).sum()
-    prediction_loss = (prediction_loss_on_source + prediction_loss_on_target) / n_known
+    prediction_loss_on_src = crossentropy(class_logits_on_src, true_labels_on_src)
+    prediction_loss_on_trg = crossentropy(class_logits_on_trg, true_labels_on_trg)
+    n_known = (true_labels_on_src != unk_value).sum() + \
+              (true_labels_on_trg != unk_value).sum()
+    prediction_loss = (prediction_loss_on_src + prediction_loss_on_trg) / n_known
 
     binary_crossentropy = torch.nn.BCEWithLogitsLoss(reduction='sum')
-    domain_loss_on_source = binary_crossentropy(logprobs_target_on_source, is_target_on_source)
-    domain_loss_on_target = binary_crossentropy(logprobs_target_on_target, is_target_on_target)
-    domain_loss = (domain_loss_on_source + domain_loss_on_target) / (source_len + target_len)
+    domain_loss_on_src = binary_crossentropy(logprobs_target_on_src, is_target_on_src)
+    domain_loss_on_trg = binary_crossentropy(logprobs_target_on_trg, is_target_on_trg)
+    domain_loss = (domain_loss_on_src + domain_loss_on_trg) / (source_len + target_len)
     loss = domain_loss_weight * domain_loss \
            + prediction_loss_weight * prediction_loss
     return loss, {
-        "domain_loss_on_source": domain_loss_on_source,
-        "domain_loss_on_target": domain_loss_on_target,
+        "domain_loss_on_src": domain_loss_on_src,
+        "domain_loss_on_trg": domain_loss_on_trg,
         "domain_loss": domain_loss,
-        "prediction_loss_on_source": prediction_loss_on_source,
-        "prediction_loss_on_target": prediction_loss_on_target,
+        "prediction_loss_on_src": prediction_loss_on_src,
+        "prediction_loss_on_trg": prediction_loss_on_trg,
         "prediction_loss": prediction_loss
     }
 
@@ -121,30 +121,30 @@ def loss_DANN(model,
     :return:
         loss: torch.Tensor,
         losses dict:{
-            "domain_loss_on_source"
-            "domain_loss_on_target"
+            "domain_loss_on_src"
+            "domain_loss_on_trg"
             "domain_loss"
-            "prediction_loss_on_source"
-            "prediction_loss_on_target"
+            "prediction_loss_on_src"
+            "prediction_loss_on_trg"
             "prediction_loss"
         }
     """
     model_output = model.forward(batch['src_images'])
-    class_logits_on_source = model_output['class']
-    logprobs_target_on_source = model_output['domain']
+    class_logits_on_src = model_output['class']
+    logprobs_target_on_src = model_output['domain']
 
     model_output = model.forward(batch['trg_images'])
-    class_logits_on_target = model_output['class']
-    logprobs_target_on_target = model_output['domain']
+    class_logits_on_trg = model_output['class']
+    logprobs_target_on_trg = model_output['domain']
 
     domain_loss_weight = calc_domain_loss_weight(epoch, n_epochs)
     prediction_loss_weight = calc_prediction_loss_weight(epoch, n_epochs)
     return _loss_DANN_splitted(
-        class_logits_on_source,
-        class_logits_on_target,
-        logprobs_target_on_source,
-        logprobs_target_on_target,
-        true_labels_on_source=batch['src_classes'],
-        true_labels_on_target=batch['trg_classes'],
+        class_logits_on_src,
+        class_logits_on_trg,
+        logprobs_target_on_src,
+        logprobs_target_on_trg,
+        true_labels_on_src=batch['src_classes'],
+        true_labels_on_trg=batch['trg_classes'],
         domain_loss_weight=domain_loss_weight,
         prediction_loss_weight=prediction_loss_weight)
