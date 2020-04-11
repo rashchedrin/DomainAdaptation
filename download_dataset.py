26ea95519d34196a3929fbb10c69a04b044aa590
Roman Shchedrin <rashchedrin@gmail.com>
2020-04-11 13:04:43 +0300

    changed path

--- download_dataset.py
+++ download_dataset.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     # TAKE ID FROM SHAREABLE LINK
     URL = "https://drive.google.com/uc?id=0B4IapRTv9pJ1WGZVd1VDMmhwdlE"
     # DESTINATION FILE ON YOUR DISK
-    DESTINATION = "data/domain_adaptation_images.tar.gz"
+    DESTINATION = "data/office-31/domain_adaptation_images.tar.gz"
     MD5_HASH = '1b536d114869a5a8aa4580b89e9758fb'
     gdown.cached_download(URL,
                           DESTINATION,
